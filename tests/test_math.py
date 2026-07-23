a2aa364ef4773fb563da9ff335dbef0a687c19b9
shahzaib rehman <nk780877@gmail.com>
2026-07-23 15:18:58 +0500

    Clarify add() intent (still failing)

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,7 +1,8 @@
 def add(a, b):
-    # BUG: should add the two numbers, but subtracts
+    # BUG: should add the two numbers, but subtracts them
     return a - b
 
 
 def test_add():
+    # 2 + 3 must equal 5
     assert add(2, 3) == 5
